bd4520ab64f3d01479b5b6fd7c5687fa4150f3b5
Robert Magill <r@magill.xyz>
2021-11-14 15:28:50 -0500

    master: add memory, removed tasks

--- config.h
+++ config.h
@@ -3,14 +3,15 @@ static const Block blocks[] = {
 	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
 	/* {"⌨", "sb-kbselect", 0, 30}, */
 	{"", "cat /tmp/recordingicon 2>/dev/null",	0,	9},
-	{"",	"sb-tasks",	10,	26},
 	{"",	"sb-music",	0,	11},
+	{"",	"sb-memory",	10,	14},
 	{"",	"sb-mailbox",	180,	12},
 	{"",	"sb-volume",	0,	10},
 	{"",	"sb-battery",	5,	3},
 	{"",	"sb-internet",	5,	4},
 	{"",	"sb-clock",	60,	1},
 	{"",	"sb-current-temp",	300,	5},
+	/* {"",	"sb-tasks",	10,	26}, */
 	/* {"",	"sb-pacpackages",	0,	8}, */
 	/* {"",	"sb-news",		0,	6}, */
 	/* {"",	"sb-crypto",	0,	13}, */
@@ -21,7 +22,6 @@ static const Block blocks[] = {
 	/* {"",	"sb-price eth Ethereum 🍸",	9000,	23}, */
 	/* {"",	"sb-price btc Bitcoin 💰",				9000,	21}, */
 	/* {"",	"sb-torrent",	20,	7}, */
-	/* {"",	"sb-memory",	10,	14}, */
 	/* {"",	"sb-cpu",		10,	18}, */
 	/* {"",	"sb-moonphase",	18000,	17}, */
 	/* {"",	"sb-forecast",	18000,	5}, */
